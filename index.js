0d8011faaae8a41a418b4cd846b7df1f3c71a0cd
Brandon Lee Truter <brandon.truter@ncino.com>
2024-12-04 13:05:27 +0200

    add mutations, subscriptions and documentation

--- index.js
+++ index.js
@@ -1,19 +1,66 @@
+const { Query } = require("mongoose");
 const { ApolloServer, gql } = require("apollo-server");
 
 const typeDefs = gql`
+  scalar Date
+
+  """
+  A learning day based on the relevant topics
+  """
   type LearningDay {
+    "A learning days' unique identifier"
     id: ID!
-    date: String!
-    mountain: String!
+    "The date of the learning day"
+    date: Date!
+    "The site for the Topics"
+    site: String!
+    "The topics covered on the learning day"
+    topics: Topics
+  }
+
+  enum Topics {
+    GRAPHQL
+    RAILS
+    VUE
   }
 
   type Query {
     totalDays: Int!
-    allLearningDays: [LearningDay!]!
+    allDays: [LearningDay!]!
+  }
+
+  input AddDayInput {
+    date: Date!
+    site: String!
+    topics: Topics!
+  }
+
+  type RemoveDayPayload {
+    day: LearningDay!
+    removed: Boolean!
+    totalBefore: Int
+    totalAfter: Int
+  }
+
+  type Mutation {
+    addDay(input: AddDayInput!): LearningDay!
+    removeDay(id: ID!): RemoveDayPayload!
+  }
+
+  type Subscription {
+    newDay: LearningDay!
   }
 `;
 
-const server = new ApolloServer({ typeDefs, mocks: true });
+const mocks = {
+  Date: () => "10/2/2025",
+  String: () => "Viva Learning",
+  Query: () => ({
+    allDays: () => new Array(5).fill(),
+  }),
+};
+
+const server = new ApolloServer({ typeDefs, mocks });
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
